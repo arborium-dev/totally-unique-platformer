645b825c13415f13762777114df472dec9fc3156
Nathaniel <nathaniel@somaradio.ca>
2026-04-22 18:41:32 -0400

    more movment
    THeres bugs please fix

--- Assets/movment.cs
+++ Assets/movment.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,9 +6,11 @@ public class movment : MonoBehaviour
 {
     public float speed;
     public float jumpForce;
-
+    public Vector2 boxSize;
     InputAction moveAction;
     InputAction jumpAction;
+    public float distance;
+    public LayerMask groundLayer;
 
     public Rigidbody2D _rb;
     private void Start()
@@ -16,15 +19,37 @@ public class movment : MonoBehaviour
         jumpAction = InputSystem.actions.FindAction("Jump");
         _rb = GetComponent<Rigidbody2D>();
     }
+
+    public bool isGrounded() 
+    { 
+        if(Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, distance, groundLayer))
+        {
+            return true;
+        }
+        else 
+        { 
+            return false; 
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position -transform.up * distance, boxSize);
+    }
     void Update()
     {
-       
+        
+        
+        
+
         Vector2 moveValue = moveAction.ReadValue<Vector2>();
         _rb.linearVelocity = new Vector2(moveValue.x * speed, _rb.linearVelocity.y);
 
-        if (jumpAction.IsPressed())
+        
+        if (jumpAction.IsPressed()&& isGrounded())
         {
             _rb.AddForce(transform.up * jumpForce);
         }
+        
     }
 }
